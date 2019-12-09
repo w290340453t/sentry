70427f6c688d915f9548ed5c97c36ed6638100d6
Joshua Li <josh@jrl.ninja>
2019-12-09 12:35:03 -0800

    remove these methods which aren't called in django 1.10
    (cherry picked from commit 0654551056b3bba69531fe8bd4208130e9414e5d)

--- src/bitfield/models.py
+++ src/bitfield/models.py
@@ -129,24 +129,6 @@ class BitField(BigIntegerField):
             value = value.mask
         return int(value)
 
-    def get_db_prep_lookup(self, lookup_type, value, connection, prepared=False):
-        if isinstance(getattr(value, "expression", None), Bit):
-            value = value.expression
-        if isinstance(value, (BitHandler, Bit)):
-            return [value.mask]
-        return BigIntegerField.get_db_prep_lookup(
-            self, lookup_type=lookup_type, value=value, connection=connection, prepared=prepared
-        )
-
-    def get_prep_lookup(self, lookup_type, value):
-        if isinstance(getattr(value, "expression", None), Bit):
-            value = value.expression
-        if isinstance(value, Bit):
-            if lookup_type in ("exact",):
-                return value
-            raise TypeError("Lookup type %r not supported with `Bit` type." % lookup_type)
-        return BigIntegerField.get_prep_lookup(self, lookup_type, value)
-
     def to_python(self, value):
         if isinstance(value, Bit):
             value = value.mask
